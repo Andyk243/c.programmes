4015c4b62e4755cae61446d9f77be5675ac88094
Andyk243 <keraraandy@gmail.com>
2026-09-21 02:18:48 +0300

    Add files via upload

---
+++ volume and surface area calculator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    float radius, height, volume, surfaceArea;
+    const float pi = 3.14159;
+
+    // Ask the user to enter radius
+    printf("Enter the radius of the cylinder: ");
+    scanf("%f", &radius);
+
+    // Ask the user to enter height
+    printf("Enter the height of the cylinder: ");
+    scanf("%f", &height);
+
+    // Calculate volume
+    volume = pi * radius * radius * height;
+
+    // Calculate surface area
+    surfaceArea = 2 * pi * radius * (radius + height);
+
+    // Display results
+    printf("Volume of the cylinder = %.2f\n", volume);
+    printf("Surface area of the cylinder = %.2f\n", surfaceArea);
+
+    return 0;
+}
